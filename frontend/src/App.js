eb1e032e5cf60dc2ebb807a4850dc616e2fcf3a7
Mini-Valeria <anavaleriap257@gmail.com>
2025-03-07 09:24:17 -0600

    Archivo Users cambiado a SignUp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Navbar } from "./components/Navbar";
 import { About } from "./components/About";
-import { Users } from "./components/Users";
 import { SignUp } from "./components/SignUp";
 
 function App() {
